5193cf462cfb0b547cffd59b24b9bf6c22bd2aef
Ever <tlh_07@163.com>
2025-10-03 17:03:30 +0800

    feat: Clean up codebase from unused assets
    Removed dead code and redundant assets to improve overall project health.
    
    - Deleted `server.js`, `code.html`, `constants.ts`, and `services/database.js`.
    - Uninstalled the unused `ts-node` dependency.
    - Deleted the unused `KeyIcon.tsx` and `NewspaperIcon.tsx` icon files.

--- components/icons/KeyIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const KeyIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2} {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H5v-2H3v-2H1v-4a6 6 0 017.743-5.743z" />
-    </svg>
-);
--- components/icons/NewspaperIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const NewspaperIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4 12H9m-4 0H5m4 0v-4m0 4h4m-4 0H9m4 0v-4m0 4h4m-4 0H9" />
-    </svg>
-);
